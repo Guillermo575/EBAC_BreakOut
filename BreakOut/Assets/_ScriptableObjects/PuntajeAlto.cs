e4961284ecce035c277a24a749ba5f96ed0e592a
Guillermo575 <30479672+Guillermo575@users.noreply.github.com>
2024-02-06 00:55:10 -0600

    Lecciones 3.23.2 y 3.233

---
+++ BreakOut/Assets/_ScriptableObjects/PuntajeAlto.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "PuntajeAlto", menuName = "Tools/PuntajeAlto", order = 0)]
+public class PuntajeAlto : PuntajePersistente
+{
+    public int puntajeAlto = 10000;
+    public int puntaje = 0;
+}
---
+++ BreakOut/Assets/_Scripts/Puntaje.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+public class Puntaje : MonoBehaviour
+{
+    Transform transformPuntajeAlto;
+    Transform transformPuntajeActual;
+    TMP_Text textoPuntajeAlto;
+    TMP_Text textoPuntajeActual;
+    public PuntajeAlto puntajeAltoSO;
+    void Start()
+    {
+        puntajeAltoSO.Cargar();
+        transformPuntajeAlto = GameObject.Find("PuntajeAlto").transform;
+        transformPuntajeActual = GameObject.Find("PuntajeActual").transform;
+        textoPuntajeAlto = transformPuntajeAlto.GetComponent<TMP_Text>();
+        textoPuntajeActual = transformPuntajeActual.GetComponent<TMP_Text>();
+        textoPuntajeAlto.text = $"Puntaje Alto: {puntajeAltoSO.puntajeAlto}";
+        puntajeAltoSO.puntaje = 0;
+    }
+    private void FixedUpdate()
+    {
+        puntajeAltoSO.puntaje += 50;
+    }
+    void Update()
+    {
+        textoPuntajeActual.text = $"Puntaje: {puntajeAltoSO.puntaje}";
+        if (puntajeAltoSO.puntaje > puntajeAltoSO.puntajeAlto)
+        {
+            puntajeAltoSO.puntajeAlto = puntajeAltoSO.puntaje;
+            textoPuntajeAlto.text = $"Puntaje Alto: {puntajeAltoSO.puntajeAlto}";
+            puntajeAltoSO.Guardar();
+        }
+    }
+}
