ff8c83da1446d9e2d63f5a0a18204d0d9be38bfa
Guillermo575 <30479672+Guillermo575@users.noreply.github.com>
2024-02-18 00:59:19 -0600

    ajustes gamemamager

--- BreakOut/Assets/_Scripts/GameManager.cs
+++ BreakOut/Assets/_Scripts/GameManager.cs
@@ -7,7 +7,32 @@ public class GameManager : MonoBehaviour
 {
     public event EventHandler OnGameStart;
     public event EventHandler OnGamePause;
+    public event EventHandler OnGameResume;
     public event EventHandler OnGameEnd;
     public event EventHandler OnGameOver;
+    public event EventHandler OnGameExit;
     public event EventHandler OnGameLevelCleared;
+
+    private void Awake()
+    {
+        OnGameStart += delegate { Time.timeScale = 1; };
+        OnGamePause += delegate { Time.timeScale = 0; };
+        OnGameResume += delegate { Time.timeScale = 1; };
+        OnGameEnd += delegate { Time.timeScale = 0; };
+        OnGameOver += delegate { Time.timeScale = 0; };
+        OnGameExit += delegate { Time.timeScale = 1; };
+        OnGameLevelCleared += delegate { Time.timeScale = 0; };
+    }
+    public void PauseGame()
+    {
+        OnGamePause?.Invoke(this, EventArgs.Empty);
+    }
+    public void ResumeGame()
+    {
+        OnGamePause?.Invoke(this, EventArgs.Empty);
+    }
+    public void ExitGame()
+    {
+        OnGameExit?.Invoke(this, EventArgs.Empty);
+    }
 }
