0fa2339e683d64431ae0b41ae78f829357684900
Guillermo575 <30479672+Guillermo575@users.noreply.github.com>
2024-02-26 23:08:54 -0600

    scriptableobject textura ruptura

---
+++ BreakOut/Assets/_ScriptableObjects/BloqueTexturaRuptura.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "BloqueTexturaRuptura", menuName = "Tools/Bloque Textura Ruptura", order = 2)]
+public class BloqueTexturaRuptura : ScriptableObject
+{
+    public Texture2D[] texturasGrietas;
+}
