03480e6f169107b05f2b9543e12fc53b9d1f20d6
Guillermo575 <30479672+Guillermo575@users.noreply.github.com>
2024-02-26 23:10:04 -0600

    ajustes bloques

--- BreakOut/Assets/_Scripts/Gameplay/Bloque/Bloque.cs
+++ BreakOut/Assets/_Scripts/Gameplay/Bloque/Bloque.cs
@@ -7,10 +7,12 @@ public class Bloque : MonoBehaviour
 {
     public int resistencia = 1;
     public UnityEvent AumentarPuntaje;
+    public BloqueTexturaRuptura objTexturaRuptura;
     MathRNG objMathRNG = new MathRNG(45289574);
-
+    Renderer m_Renderer;
     void Start()
     {
+        m_Renderer = this.gameObject.GetComponent<Renderer>();
     }
     void Update()
     {
@@ -26,16 +28,23 @@ public class Bloque : MonoBehaviour
         direccion = direccion.normalized;
         collision.rigidbody.velocity = collision.gameObject.GetComponent<Bola>().velocidadBola * direccion;
         Vector3 v = collision.rigidbody.velocity;
-        v.x = v.x < 1 && v.x > -1 ? Random.Range(-v.y, v.y) : v.x;
-        v.y = v.y < 1 && v.y > -1 ? Random.Range(-v.x, v.x) : v.y;
+        v.x = v.x < 1 && v.x > -1 ? objMathRNG.NextValueFloat(-v.y, v.y) : v.x;
+        v.y = v.y < 1 && v.y > -1 ? objMathRNG.NextValueFloat(-v.x, v.x) : v.y;
         collision.rigidbody.velocity = v;
         resistencia--;
+    }
+    public virtual void CambiarTextura()
+    {
+        var materialObj = m_Renderer.material;
+        var texturaObj = objTexturaRuptura.texturasGrietas[0];
+        materialObj.SetTexture("_EmissionMap", texturaObj);
     }
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Bola")
         {
             RebotarBola(collision);
+            CambiarTextura();
         }
     }
 }
