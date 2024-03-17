c49d140ba9a4591cf18fb846beae37cc0ae5c3c5
Guillermo575 <30479672+Guillermo575@users.noreply.github.com>
2024-03-17 00:52:00 -0600

    ajustes bloque premio script

--- BreakOut/Assets/_Scripts/Gameplay/Bloque/Bloque_Premio.cs
+++ BreakOut/Assets/_Scripts/Gameplay/Bloque/Bloque_Premio.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Bloque_Premio : Bloque
 {
+    public string Clave = "LIFE";
+    private GameObject gameManager;
     void OnDestroy()
     {
-        //La idea que tengo con este bloque es que cuando se destruya
-        //deje caer un objeto que al tocar el jugador le de una bonificacion como:
-        //Mas puntos, una vida extra, etc.
+        gameManager = GameObject.Find("GamePlayManager");
+        gameManager.GetComponent<AdministradorPowerUps>().CallPowerUp(Clave);
     }
 }
